f4ff12e5d7a23c6b691ed997950e2715648cb7ab
Andy Wozniak <136962272+ljf74@users.noreply.github.com>
2023-12-30 12:06:09 +0000

    Delete 1.sh

--- 1.sh
+++
@@ -1,6 +0,0 @@
-# cd lesson2 && npx tailwindcss init
-cd l*1 && npx tailwindcss -i ./src/input.css -o ./build/css/style.css
-cd l*2/build && python3 -m http.server && cd ..
-# git clone https://github.com/gitdagray/tailwind-css-course.git
-# rm -rf  /home/runner/tailwindcss/.config
-# cd l*2 && npm run tailwind
